c0943155ebff00431cf2eff7f5660011ee1d8bb1
shahriar7ahmed <shahriar7ahmed@gmail.com>
2022-10-20 11:12:56 +0600

    SignUp D

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,11 @@ import { AuthContext } from '../contexts/UserContext';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const { user } = useContext(AuthContext)
+    const { user, logout } = useContext(AuthContext)
+    const handelLogout = () => {
+        logout()
+            .then(() => { })
+    }
     return (
         <div className="bg-slate-600">
             <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
@@ -76,9 +80,9 @@ const Header = () => {
                                 About us
                             </NavLink>
                         </li>
-                        {user ? <li>
+                        {user && user?.uid ? <li>
                             <button
-                              
+                              onClick={handelLogout}
                                 className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                                 aria-label="Log Out"
                                 title="Log Out"
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,20 +1,35 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
 import app from '../firebase/firebase.config';
 export const AuthContext = createContext()
 
 const auth = getAuth(app)
 
 const UserContext = ({ children }) => {
-    const [user, setUser] = useState('')
-
+    const [user, setUser] = useState({})
+    const googleProvider = new GoogleAuthProvider();
     const signup = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
     const profileUpdate = name => {
         return updateProfile(auth.currentUser, { displayName: name })
     }
-    const authInfo = { user, signup, profileUpdate }
+    const googleLogin = () => {
+        return signInWithPopup(auth,googleProvider)
+    }
+
+    const logout = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+            return ()=> unsubscribe()
+        })
+  },[])
+
+    const authInfo = { user, signup, profileUpdate,googleLogin,logout }
 
     return (<AuthContext.Provider value={authInfo} >
         {children}
